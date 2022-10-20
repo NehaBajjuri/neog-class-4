41db4161777ae3495d8a4fb175776775321956c9
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2022-10-20 17:15:56 +0530

    output ka cutout

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
 console.log(txtInput)
 btnTranslate.addEventListener("click",function clickEventHandler(){
     console.log("clicked!");
